0e6d030e6da15ee8527a55a569d814e0c8052ba8
Valerasan <yarovoy.valera2002@gmail.com>
2025-09-08 13:19:45 +0300

    change component struct, transferring the logic of notes to another computer

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -1,75 +1,21 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note";
-import CreateArea from "./CreateArea";
+import NotesBoard from "./NotesBoard";
+
+axios.defaults.withCredentials = true;
 
 function App() {
-    const [notes, setNotes] = useState([]);
-
-    const [message, setMessage] = useState({})
-
-
-
-    useEffect(() => {
-        fetch("http://localhost:3000/api/notes")
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                setNotes(data)
-            })
-            .catch(err => console.log(err));
-    }, []);
-
-
-    function addNote(newNote) {
-        setNotes(prevNotes => {
-            return [...prevNotes, newNote];
-        });
-    }
-
-    async function deleteNote(id) {
-        try {
-            const response = await fetch("http://localhost:3000/api/notes/delete", {
-                method: "DELETE",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ id }),
-            });
-
-            const data = await response.json();
-
-            setNotes(prevNotes => {
-                return prevNotes.filter((noteItem) => {
-                    return noteItem.id !== id;
-                });
-            });
-
-        } catch (error) {
-            console.error(error);
-            console.log("Error");
-        }
-
-    }
-
+    
+    const [user, setUser] = useState(null);
+    const [form, setForm] = useState({ email: "", password: "" });
 
 
     return (
         <div>
             <Header />
-
-            <CreateArea onAdd={addNote} />
-            {notes.map((noteItem, index) => {
-                return (
-                    <Note
-
-                        key={index}
-                        id={noteItem.id}
-                        title={noteItem.title}
-                        content={noteItem.content}
-                        onDelete={deleteNote}
-                    />
-                );
-            })}
+                <NotesBoard />
             <Footer />
         </div>
     );
---
+++ Frontend/src/components/NotesBoard.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+
+
+function NotesBoard() {
+
+    const [notes, setNotes] = useState([]);
+
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/notes")
+            .then(res => setNotes(res.data))
+            .catch(err => console.log(err));
+
+    }, []);
+
+
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
+    async function deleteNote(id) {
+        try {
+            const response = await axios.delete("http://localhost:3000/api/notes/delete", {
+                data: { id }, 
+                headers: { "Content-Type": "application/json" },
+            });
+
+
+            const data = response.data;
+
+            setNotes(prevNotes => {
+                return prevNotes.filter((noteItem) => {
+                    return noteItem.id !== id;
+                });
+            });
+
+        } catch (error) {
+            console.error(error);
+            console.log("Error");
+        }
+
+    }
+
+    return (<div>
+        <CreateArea onAdd={addNote} />
+        {notes.map((noteItem, index) => {
+            return (
+                <Note
+
+                    key={index}
+                    id={noteItem.id}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    onDelete={deleteNote}
+                />
+            );
+        })}
+
+    </div>)
+}
+
+
+export default NotesBoard;
